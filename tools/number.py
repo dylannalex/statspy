6e1630b516a908293dbd865d285d3d416d2c7623
Dylan Tintenfich <tintenfichdylan@gmail.com>
2022-04-04 10:16:33 -0300

    Create tools

---
+++ tools/number.py
@@ -0,0 +1,7 @@
+def truncate(number: float, after_comma_digits: int):
+    n = str(number)
+    comma_index = n.find(".")
+    decimals = len(n[comma_index + 1 :])
+    if decimals < after_comma_digits:
+        return n + "".join(["0" for _ in range(after_comma_digits - decimals)])
+    return n[0 : comma_index + after_comma_digits + 1]
