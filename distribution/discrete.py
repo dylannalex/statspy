45d80ea4ded0d40f22c3b600621b4689010370d2
Dylan Tintenfich <tintenfichdylan@gmail.com>
2022-05-03 13:02:06 -0300

    Add hypergeometric distribution

--- distribution/discrete.py
+++ distribution/discrete.py
@@ -10,3 +10,16 @@ def binomial(k, n, p):
     q = 1 - p
     comb = tools.binomial_coefficient(n, k)
     return comb * (p**k) * (q ** (n - k))
+
+
+def hypergeometric(x, N, n, k):
+    """
+    x: number of successes in n trials
+    N: number of trials
+    n: number of trials chosen from N
+    k: number of successes in N trials
+    """
+    success_comb = tools.binomial_coefficient(k, x)
+    failure_comb = tools.binomial_coefficient(N - k, n - x)
+    total_comb = tools.binomial_coefficient(N, n)
+    return success_comb * failure_comb / total_comb
